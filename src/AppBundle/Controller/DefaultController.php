8119abd526b6d849ce6ce4e748788d7fc4e5687a
Arkansio <arkansio@arkanse.fr>
2018-06-02 18:17:50 +0200

    render in controller, add billetterie

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,11 +12,17 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function index(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/indexWebsite.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index_website.html.twig');
+    }
+
+    /**
+     * @Route("/billetterie/buy", name="billetterie")
+     */
+    public function billetterie(Request $request)
+    {
+        return $this->render('default/navbar.html.twig');
     }
 }
